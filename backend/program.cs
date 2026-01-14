1546e3312206154e1d92cefbd68c0d0feb32b9db
trazll <degleted@gmail.com>
2026-01-14 09:37:33 +0300

    Added basic backend program

---
+++ backend/program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Меня зовут Марычев Е. Н.");
+Console.WriteLine(DateTime.Now);
