ee9db27a7a1c2fd22e1c73b62ca1c2bf523da85b
traz <degleted@gmail.com>
2026-02-02 22:57:26 +0300

    ввод имени

--- backend/program.cs
+++ backend/program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Меня зовут Марычев Е. Н.");
-Console.WriteLine(DateTime.Now);
+// Console.WriteLine("Hello, World!");
+// Console.WriteLine("Меня зовут Марычев Е. Н.");
+// Console.WriteLine(DateTime.Now);
+Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}!");
